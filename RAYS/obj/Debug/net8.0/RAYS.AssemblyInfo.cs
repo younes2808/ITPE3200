d0e53f4a0a5ee1bf12dac53ebee6158fedb0d3f0
younes2808 <benhaida.mehdi@gmail.com>
2024-09-18 10:23:33 +0200

    Kjører dot net build

--- RAYS/obj/Debug/net8.0/RAYS.AssemblyInfo.cs
+++ RAYS/obj/Debug/net8.0/RAYS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RAYS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5577174d22f66210862ced9edc8131f400259a6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38a452cd778ce03fedd53ef0ad680e55b6c0fc3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RAYS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RAYS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
