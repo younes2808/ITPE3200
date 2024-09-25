e6ac8b0f806cdf9e86d922b1f76357a4c9bf6049
S374917 <115645489+S374917@users.noreply.github.com>
2024-09-25 16:18:22 +0200

    Laget en postbar

--- clientapp/src/App.jsx
+++ clientapp/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'; // Import the CSS file that includes Tailwind directives
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import './Styles/Rays.css'
+import TestPage from './Pages/TestPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/test" element={<TestPage/>}/>
       </Routes>
     </Router>
   );
