103907c6481cd9883b59bfb19d20c7e6f81adfa7
R4f3y <115638319+R4f3y@users.noreply.github.com>
2024-09-25 16:37:49 +0200

    fikset path og router

--- clientapp/src/App.jsx
+++ clientapp/src/App.jsx
@@ -1,22 +1,16 @@
 import './App.css'; // Import the CSS file that includes Tailwind directives
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import RightNavbar from './Components/RightNavbar';
-import LeftNavbar from './Components/LeftNavbar';
+import HomePage from './Pages/HomePage';
+import Feed from './Pages/Feed';
 import './Styles/Rays.css';
 
 function App() {
   return (
     <Router>
-      <div className="flex h-screen">
-        {/* Left Navbar */}
-        <LeftNavbar />
-        {/* Main content */}
-        <div className="w-4/6 bg-gray-100 p-6 flex justify-center items-center">
-          <div className="text-center text-2xl font-semibold">Main content</div>
-        </div>
-        {/* Right Navbar */}
-        <RightNavbar />
-      </div>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/feed' element={<Feed />} />
+      </Routes>
     </Router>
   );
 }
