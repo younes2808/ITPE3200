a42121295c1dd749c9f2290f93821f425267099d
Younes Benhaida <77924726+younes2808@users.noreply.github.com>
2024-10-10 11:50:04 +0200

    Update App.jsx
    Sletter testpage

--- clientapp/src/App.jsx
+++ clientapp/src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'; // Import the CSS file that includes Tailwind directives
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import Feed from './Pages/Feed';
-import TestPage from './Pages/TestPage';
 import Register from './Pages/Register';
 import Profile from './Pages/Profile';
 import CommentsPage from './Pages/CommentsPage'; 
@@ -18,7 +17,6 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/feed' element={<Feed />} />
-        <Route path="/test" element={<TestPage/>}/>
         <Route path="/comments/:postId" element={<CommentsPage />} /> {/* Route to CommentsPage */}
         <Route path="/register" element={<Register />} />
         <Route path="/profile/:userId" element={<Profile />} />
