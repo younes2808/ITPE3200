b5ebcd55a39fdff748769329dfe9c706d744cdc9
younes2808 <benhaida.mehdi@gmail.com>
2024-10-16 16:55:27 +0200

    Har fikset responsivitet på profil-siden. Den kan nå passe Galaxy Fold Z. Liten endring i postcontrolleren for å gjøre at de siste postene til brukeren kommer opp først.

--- clientapp/src/Components/ShowFriends.jsx
+++ clientapp/src/Components/ShowFriends.jsx
@@ -44,11 +44,11 @@ const ShowFriends = ({ userId }) => {
   };
 
   if (loading) {
-    return <div className="text-white">Loading friends...</div>;
+    return <div className="text-white h-[100vw]">Loading friends...</div>;
   }
 
   if (friendDetails.length === 0) {
-    return <div className="text-white">No friends found.</div>; // Display message if no friends
+    return <div className="text-white h-[100vw]">No friends found.</div>; // Display message if no friends
   }
 
   return (
