ba3833dad52b59ba6cfbefb484059c7d0a78b19d
younes2808 <benhaida.mehdi@gmail.com>
2024-10-17 17:06:27 +0200

    Fikset et par bugs: -Friendrequests.jsx filtrerer ikke ut friend request hvor brukeren er den som sender. bottom navbar hadde et par variabler som ikke ble brukt. Left navbar fikk et par nye effekter. search-siden har fått nytt design

--- clientapp/src/Pages/Search.jsx
+++ clientapp/src/Pages/Search.jsx
@@ -7,13 +7,13 @@ import BottomNavbar from './../Components/BottomNavbar';
 
 const Search = () => {
   return (
-    <div className="bg-orange-500 flex h-screen flex-col overflow-y-auto">
+    <div className="bg-gray-800 flex h-screen flex-col overflow-y-auto">
       {/* Top Bar */}
       <TopBar />
 
       {/* Hovedcontainer */}
         {/* Justert container med dynamisk margin og max-w for å sikre plass til navbarene */}
-        <div className="flex w-full max-w-[1300px] mx-auto bg-blue-500 h-full">
+        <div className="flex w-full max-w-[1300px] mx-auto h-full border-l-2 border-r-2 border-gray-700">
 
           {/* Left Navbar - plasseres på venstre side */}
           <div className="flex-none"> 
@@ -21,7 +21,7 @@ const Search = () => {
           </div>
 
           {/* Search i midten */}
-          <div className="flex-grow bg-gray-200 p-6 overflow-y-auto w-full 400px:pl-6 400px:pr-10 510px:pl-24 510px:pr-5 580px:pr-10 md:pl-44 md:pr-16 870px:pl-48 870px:pr-20 970px:pr-16 1150px:pl-64 1150px:pr-14">
+          <div className="flex-grow bg-gray-900 p-6 overflow-y-auto w-full 400px:pl-6 scrollbar-none 510px:pl-24 510px:pr-5 580px:pr-10 md:pl-44 md:pr-16 870px:pl-48 870px:pr-20 970px:pr-16 1150px:pl-64 1150px:pr-10">
           <SearchUsers />
           </div>
 
